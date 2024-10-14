926043f2895a70e4904de71992482d96965acfc8
Joseph2912 <elsjoseph@hotmail.com>
2024-10-13 19:30:21 -0500

    listas y claves

--- react-course/src/App.tsx
+++ react-course/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import { QueMensaje } from "./Mensaje";
+import { QueMensaje4 } from "./Mensaje";
 
 //* Componentes funcionales, existen otros que son los componentes de clase pero ya casi no se usan debido a que requiere constructor y mas cosas entonces cada vez se usa menos
 const App = () => {
-  return <QueMensaje mensajeA="hola" mensajeB="adios" />;
+  return <QueMensaje4 mensajeA3={["hola", "hey", "nose"]} />;
+  <QueMensaje mensajeA="hola" mensajeB="adios" />;
 };
 
 export default App;
--- react-course/src/Mensaje.tsx
+++ react-course/src/Mensaje.tsx
@@ -36,3 +36,40 @@ export const QueMensaje2 = ({ mensajeA, mensajeB }: mensajeProps) => {
     <h1 onClick={cambiarMensajeA}>{mostrarMensajeA ? mensajeA : mensajeB}</h1>
   );
 };
+
+//* Renderizacion condicional otra manera
+
+interface mensajeProps2 {
+  mensajeA2?: string;
+  etiqueta?: string;
+}
+
+export const QueMensaje3 = ({ mensajeA2, etiqueta }: mensajeProps2) => {
+  if (!mensajeA2) return null;
+  switch (etiqueta) {
+    case "h1": {
+      return <h1> {mensajeA2}</h1>;
+    }
+    case "h2": {
+      return <h2> {mensajeA2}</h2>;
+    }
+    case "h3": {
+      return <h3> {mensajeA2}</h3>;
+    }
+    case "h4": {
+      return <h4> {mensajeA2}</h4>;
+    }
+    default:
+      return <span>{mensajeA2}</span>;
+  }
+};
+
+//* Listas y claves
+
+interface mensajeProps3 {
+  mensajeA3: string[];
+}
+
+export const QueMensaje4 = ({ mensajeA3 }: mensajeProps3) => {
+  return mensajeA3.map((mensajeA3) => <h1 key={mensajeA3}>{mensajeA3}</h1>);
+};
